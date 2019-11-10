dcc809aba0ea7fea4e9202eb5119ca9e2e51c711
moka3660 <moka12almukha30@gmail.com>
2019-11-10 22:48:23 +0900

    update 暫定処理 Megaをレベルシフタにした

--- AudioFFT2/MegaLED2/MegaLED2.ino
+++ AudioFFT2/MegaLED2/MegaLED2.ino
@@ -2,15 +2,30 @@
     Arduino Megaつかう
     SPRESENSEのsubcore (1or2未確定)からシリアルでデータ受信
     LED光らす
-
+    これはレベルシフタです
 */
+int val =0;
 
 void setup()
 {
-
+    for (int i = 30; i < 46; i++)
+    {
+        pinMode(i,OUTPUT);
+        digitalWrite(i,LOW);
+    }
 }
 
 void loop()
 {
-    
+    for (int i = 0; i < 16; i++)
+    {
+        val = analogRead(i);
+        if (val > 300)
+        {
+            digitalWrite(i+30,HIGH);
+        }else
+        {
+            digitalWrite(i+30,LOW);
+        }
+    }
 }
