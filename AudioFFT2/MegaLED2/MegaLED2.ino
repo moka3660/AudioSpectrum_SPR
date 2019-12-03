e619fd04fc896fde4c2a1d6234f52f9aed2f90bf
shibain33 <rozenzulu3660@gmail.com>
2019-12-04 08:43:29 +0900

    update MegaLED2
    1)LEDピンのイニシャル設定追加:wq

--- AudioFFT2/MegaLED2/MegaLED2.ino
+++ AudioFFT2/MegaLED2/MegaLED2.ino
@@ -4,12 +4,27 @@
     LED光らす
 
 */
+const int lowside[20] = { A8, A9, A10, A11, A12, 
+                          A13, A14, A15, 53, 51,
+                          49, 47, 45, 43, 41,
+                          39, 35, 33, 31, 29 };
+const int highside[10] = { 52, 50, 48, 46, 44,
+                           42, 40, 38, 36, 34 };
+
 byte incomingByte = 0;	// 受信データ用
 byte data[10];  //処理用データ
 void setup()
 {
     Serial.begin(115200);
     Serial1.begin(115200);
+    for (int i = 0; i != 20; i++)
+    {
+        pinMode(lowside[i],OUTPUT);
+    }
+    for (int i = 0; i != 10; i++)
+    {
+        pinMode(highside[i],OUTPUT);
+    }
     pinMode(8,OUTPUT);
     for (int i = 0; i < 10; i++)
     {
